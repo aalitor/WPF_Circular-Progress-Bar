a74860f1c143c8118c5000e2af166f14e37558e7
aalitor <alitor.enm@gmail.com>
2016-09-11 16:26:19 +0300

    Circular progress bar created in WPF
    Circular progress bar created in WPF

---
+++ CircularProgressBar.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Circular_Progress_Bar
+{
+    public partial class CircularProgressBar : ProgressBar
+    {
+        public CircularProgressBar()
+        {
+            this.ValueChanged += CircularProgressBar_ValueChanged;
+            this.SizeChanged += CircularProgressBar_SizeChanged;
+
+        }
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            if (e.Property == CircularProgressBar.RadiusProperty)
+            {
+                Width = Height = Radius * 2;
+            }
+            base.OnPropertyChanged(e);
+        }
+        void CircularProgressBar_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.Radius = Math.Min(ActualWidth, ActualHeight) / 2;
+        }
+        void CircularProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+            CircularProgressBar bar = sender as CircularProgressBar;
+            double currentAngle = bar.Angle;
+            double targetAngle = e.NewValue / bar.Maximum * 359.999;
+            double duration = Math.Abs(currentAngle - targetAngle) / 359.999 * 500;
+            DoubleAnimation anim = new DoubleAnimation(currentAngle, targetAngle, TimeSpan.FromMilliseconds(duration > 0 ? duration : 10));
+            bar.BeginAnimation(CircularProgressBar.AngleProperty, anim, HandoffBehavior.Compose);
+        }
+
+        public double Angle
+        {
+            get { return (double)GetValue(AngleProperty); }
+            set { SetValue(AngleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Angle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AngleProperty =
+            DependencyProperty.Register("Angle", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(0.0));
+
+        public double StrokeThickness
+        {
+            get { return (double)GetValue(StrokeThicknessProperty); }
+            set { SetValue(StrokeThicknessProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for StrokeThickness.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StrokeThicknessProperty =
+            DependencyProperty.Register("StrokeThickness", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(10.0));
+
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(CircularProgressBar), new PropertyMetadata("%0.00"));
+
+
+
+        public double Radius
+        {
+            get { return (double)GetValue(RadiusProperty); }
+            set { SetValue(RadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Radius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RadiusProperty =
+            DependencyProperty.Register("Radius", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(50.0));
+
+
+
+        public double InnerRadius
+        {
+            get { return (double)GetValue(InnerRadiusProperty); }
+            set { SetValue(InnerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for InnerRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InnerRadiusProperty =
+            DependencyProperty.Register("InnerRadius", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(40.0));
+
+
+    }
+}
---
+++ Converters.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Circular_Progress_Bar
+{
+    public class AngleToPointConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double angle = (double)values[0];
+            double radius = (double)values[1];
+            double stroke = (double)values[2];
+            double piang = angle * Math.PI / 180;
+
+            double px = Math.Sin(piang) * (radius - stroke / 2) + radius;
+            double py = -Math.Cos(piang) * (radius - stroke / 2) + radius;
+            return new Point(px, py);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class AngleToIsLargeConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double angle = (double)value;
+
+            return angle > 180;
+        }
+
+        public object ConvertBack(object value, Type targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class StrokeToStartPointConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double radius = (double)values[0];
+            double stroke = (double)values[1];
+            return new Point(radius, stroke / 2);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class RadiusToSizeConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double radius = (double)values[0];
+            double stroke = (double)values[1];
+            return new Size(radius - stroke / 2, radius - stroke / 2);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class RadiusToCenterConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double radius = (double)value;
+            return new Point(radius, radius);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class RadiusToDiameter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double radius = (double)value;
+            return 2 * radius;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class InnerRadiusConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double radius = (double)values[0];
+            double stroke = (double)values[1];
+
+            return radius - stroke;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
